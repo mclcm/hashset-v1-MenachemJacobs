867deb0487783144b04be5ea079b08c2114ab4cb
Menolo-Homobovanez <118155256+Menolo-Homobovanez@users.noreply.github.com>
2023-11-30 22:21:36 -0500

    tests size(), isEmpty(), contains(). Norm and edge.
    contains as written revealed huge problems with my iterator. I am reworking it.

--- tests/MyHashSetTest.java
+++ tests/MyHashSetTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MyHashSetTest {
 
-    Set mySet;
+    Set<String> mySet;
 
 
     @BeforeEach
@@ -18,5 +18,59 @@ class MyHashSetTest {
     @Test
     void emptySetsize() {
         assertEquals(0, mySet.size(), "Expected size of empty set to be zero");
+    }
+
+    @Test
+    void size_Normal(){
+        prep();
+
+        assertEquals(3, mySet.size(), "size() has failed to print true size");
+    }
+
+    @Test
+    void size_Edge_resize(){
+        prep();
+
+        assertTrue(mySet.remove("Poe"));
+        assertEquals(2, mySet.size(), "size() fails in to account for diminution");
+    }
+
+    @Test
+    void isEmpty_Edge_InAndOut(){
+        prep();
+
+        mySet.clear();
+
+        assertTrue(mySet.isEmpty(), "isEmpty() isn't returning true after a clear");
+        assertEquals(0, mySet.size(), "Size is not decrementing to zero after clear");
+    }
+
+    @Test
+    void contains_Normal(){
+        prep();
+
+        assertTrue(mySet.contains("Poe"), "contains() isn't finding added elements");
+        assertFalse(mySet.contains("Midnights so dreary"), "contains() is returning true for elements never added to set");
+    }
+
+    @Test
+    void contains_Edge_Refactor(){
+        String valToAdd;
+
+        //knowing something about the internal structure of the array made me wonder if this would work
+        for (int i = 0; i < 32; i++) {
+            valToAdd = ((Integer)i).toString();
+            mySet.add(valToAdd);
+        }
+
+        assertTrue(mySet.contains("30"));
+    }
+
+    // iterator hasNext next toArray toArray add remove containsAll addAll retainAll clear
+
+    void prep(){
+        mySet.add("Poe");
+        mySet.add("E.");
+        mySet.add("Near a raven");
     }
 }
